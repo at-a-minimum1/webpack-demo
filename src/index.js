ea39d2dfa9bb78709147f60d8eed13b8dc2ce009
Timothy Beler <timothybeler4@gmail.com>
2022-12-24 06:32:24 -0500

    Finished the Loading CSS portion of the Webpack:Asset Manager tutorial.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
 import myName from "./myName";
+import "./style.css";
 
 function component() {
 	const element = document.createElement("div");
-	const myNameElement = document.createElement("div");
 	// use your function!
+	const myNameElement = document.createElement("div");
+	myNameElement.classList.add("hello");
 
 	myNameElement.textContent = myName("Cody");
 	// Lodash, now imported by this script
