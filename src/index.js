5d7635618fea5d47d6541adf39d64b45c72c32dc
Timothy Beler <timothybeler4@gmail.com>
2022-12-31 02:57:58 -0500

    Started the output management tutorial. Onto the Setting up HtmlWebpackPlugin section.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import _ from "lodash";
 import myName from "./myName";
+import printMe from "./print";
 
 function component() {
 	const element = document.createElement("div");
+	const btn = document.createElement("button");
 	// use your function!
 	const myNameElement = document.createElement("div");
 
 	myNameElement.textContent = myName("Cody");
 	// Lodash, now imported by this script
 	// element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+	btn.innerHTML = "Click me and check the console!";
+	btn.onclick = printMe;
+	myNameElement.appendChild(btn);
 
 	return myNameElement;
 }
